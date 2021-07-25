b2c6a5423264206b4681f40c4b52e6f5a45afc25
Keyvan Fatehi <keyvanfatehi@gmail.com>
2021-07-25 13:50:36 -0700

    use a folder for yt content

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 async function main(){
     // get existing mp3
-    let existingMp3 = fs.readdirSync('.').reduce((acc, a)=>{
+    let existingMp3 = fs.readdirSync('youtube-content').reduce((acc, a)=>{
         let mat = a.match(/.+-(.+)\.mp3$/);
         if (mat) return [...acc, mat[1]];
         return acc;
@@ -20,24 +20,25 @@ async function main(){
         return true;
     })
 
-    // write it to disk for youtube-dl/ffmpeg to process...
-    fs.writeFileSync("__youtube-sources.txt", vidList.join("\n"));
+    let tempSources = __dirname+"/__youtube-sources.txt";
 
+    // write it to disk for youtube-dl/ffmpeg to process...
+    fs.writeFileSync(tempSources, vidList.join("\n"));
     try {
         await spawnAsync('youtube-dl', [
             '--write-info-json',
             '--write-annotations',
             '--write-description',
+            '-f', 'bestaudio',
             '--extract-audio',
             '--audio-format', 'mp3',
             '--no-post-overwrites',
-            '--batch-file', '__youtube-sources.txt'
-        ]);
+            '--batch-file', tempSources
+        ], {cwd: __dirname+"/youtube-content" });
     } catch (err){
         
-
-
-        
+    } finally {
+        fs.unlinkSync(tempSources);
     }
 
 
@@ -73,9 +74,9 @@ async function main(){
     fs.writeFileSync('list.m3u8', sortedMp3List.join('\n'));
 }
 
-async function spawnAsync(cmd, args=[]) {
+async function spawnAsync(cmd, args=[], opts={}) {
     return new Promise((resolve, reject)=>{
-        let proc = spawn(cmd, args);
+        let proc = spawn(cmd, args, opts);
         proc.stdout.pipe(process.stdout);
         proc.stderr.pipe(process.stderr);
         proc.on('exit', (code)=> code === 0 ? resolve() : reject());

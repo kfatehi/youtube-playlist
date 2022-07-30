8e5392c51f19ace758fc55952fa4992b1a4077b8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-07-29 19:31:00 -0700

    a web player

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const mediaDir = './youtube-content';
+const secsToHMS = (secs) => new Date(secs * 1000).toISOString().slice(11, 19);
+app.set('view engine', 'ejs');
+app.use('/media', express.static(mediaDir));
+app.get('/', (req, res, next)=>{
+  fs.readdir(mediaDir, (err, media)=>{
+    if (err) return next(err);
+    media = media.filter(m=>!/\]\.f\d\d\d/.test(m) && /\]\.mp4$/.test(m));
+    let metadata = media.map((m)=>{
+      return JSON.parse(fs.readFileSync(path.join(mediaDir,m).replace(/\.mp4$/, '.info.json')));
+    });
+    res.render('index', {
+      media,
+      metadata,
+      count: media.length,
+      totalDuration: secsToHMS(metadata.reduce((i,a)=>i+a.duration, 0))
+    });
+  });
+});
+app.listen(9000);
+

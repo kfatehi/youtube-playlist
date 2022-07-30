87deb270bb07df61ede108f1461d59e74d455720
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-07-29 21:46:22 -0700

    ability to prefer webm in the player

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const express = require('express');
 const app = express();
 const mediaDir = './youtube-content';
 const secsToHMS = (secs) => new Date(secs * 1000).toISOString().slice(11, 19);
+const preferFormat = 'webm';
+const ytMediaPattern = new RegExp(`\]\.${preferFormat}$`);
+const extPattern = new RegExp(`\.${preferFormat}$`);
 app.set('view engine', 'ejs');
 app.use('/media', express.static(mediaDir));
 app.get('/', (req, res, next)=>{
   fs.readdir(mediaDir, (err, media)=>{
     if (err) return next(err);
-    media = media.filter(m=>!/\]\.f\d\d\d/.test(m) && /\]\.mp4$/.test(m));
+    media = media.filter(m=>!/\]\.f\d\d\d/.test(m) && ytMediaPattern.test(m));
     let metadata = media.map((m)=>{
-      return JSON.parse(fs.readFileSync(path.join(mediaDir,m).replace(/\.mp4$/, '.info.json')));
+      return JSON.parse(fs.readFileSync(path.join(mediaDir,m).replace(extPattern, '.info.json')));
     });
     res.render('index', {
       media,
